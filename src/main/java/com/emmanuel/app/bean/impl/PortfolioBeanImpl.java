b6165ad1f0d863cae9979f7de02324f55810d00e
Emmanuel Mwiti <kiuguemmanu@gmail.com>
2023-11-01 08:57:54 -0400

    - Code refactor and separation of funtionalities

--- src/main/java/com/emmanuel/app/bean/impl/PortfolioBeanImpl.java
+++ src/main/java/com/emmanuel/app/bean/impl/PortfolioBeanImpl.java
@@ -42,116 +42,29 @@ public class PortfolioBeanImpl implements PortfolioBeanI {
         portfolios.add(retirementPortfolio);
 
 
-        StringBuilder html = new StringBuilder();
-        html.append("<!DOCTYPE html>");
-        html.append("<html lang=\"en\">");
-        html.append("<head>");
-        html.append("    <meta charset=\"UTF-8\">");
-        html.append("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-        html.append("    <title>Investment App Homepage</title>");
-        html.append("    <style>");
-        html.append("        body {");
-        html.append("            background-color: #f0f0f0;");
-        html.append("            font-family: Arial, sans-serif;");
-        html.append("        }");
-        html.append("        .header {");
-        html.append("            background-color: #007BFF;");
-        html.append("            color: white;");
-        html.append("            padding: 20px;");
-        html.append("            text-align: center;");
-        html.append("        }");
-        html.append("        .welcome {");
-        html.append("            text-align: center;");
-        html.append("            margin: 20px;");
-        html.append("        }");
-        html.append("        .portfolio-container {");
-        html.append("            display: flex;");
-        html.append("            justify-content: space-around;");
-        html.append("            flex-wrap: wrap;");
-        html.append("            padding: 20px;");
-        html.append("        }");
-        html.append("        .portfolio-card {");
-        html.append("            background-color: #fff;");
-        html.append("            border: 1px solid #ddd;");
-        html.append("            border-radius: 5px;");
-        html.append("            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);");
-        html.append("            margin: 10px;");
-        html.append("            width: 300px;");
-        html.append("            padding: 15px;");
-        html.append("        }");
-        html.append("        .portfolio-card h3 {");
-        html.append("            color: #007BFF;");
-        html.append("            font-size: 1.5rem;");
-        html.append("            margin: 0;");
-        html.append("        }");
-        html.append("        .portfolio-description {");
-        html.append("            color: #555;");
-        html.append("            margin: 10px 0;");
-        html.append("        }");
-        html.append("        ul {");
-        html.append("            list-style: none;");
-        html.append("            padding: 0;");
-        html.append("        }");
-        html.append("        li {");
-        html.append("            color: #333;");
-        html.append("            margin: 5px 0;");
-        html.append("        }");
-        html.append("        .performance {");
-        html.append("            display: flex;");
-        html.append("            justify-content: space-between;");
-        html.append("            align-items: center;");
-        html.append("        }");
-        html.append("        .performance p {");
-        html.append("            color: #4CAF50;");
-        html.append("            font-weight: bold;");
-        html.append("        }");
-        html.append("        .analysis-button {");
-        html.append("            background-color: #007BFF;");
-        html.append("            color: white;");
-        html.append("            border: none;");
-        html.append("            border-radius: 5px;");
-        html.append("            padding: 10px 20px;");
-        html.append("            cursor: pointer;");
-        html.append("            transition: background-color 0.3s;");
-        html.append("        }");
-        html.append("        .analysis-button:hover {");
-        html.append("            background-color: #0056b3;");
-        html.append("        }");
-        html.append("    </style>");
-        html.append("</head>");
-        html.append("<body>");
-        html.append("<div class=\"header\">");
-        html.append("    <h1>Welcome Back, Emmanuel Mwiti!</h1>");
-        html.append("    <p>Logged in at (dynamic log time and date)</p>");
-        html.append("</div>");
-        html.append("<div class=\"welcome\">");
-        html.append("    <p>This is your investment dashboard.</p>");
-        html.append("</div>");
-        html.append("<div class=\"portfolio-container\">");
+        StringBuilder cardBuilder = new StringBuilder();
+
 
         for (Portfolio portfolio : portfolios) {
-            html.append("    <div class=\"portfolio-card\">");
-            html.append("        <h3>").append(portfolio.getName()).append("</h3>");
-            html.append("        <p class=\"portfolio-description\">").append(portfolio.getName()).append(" Portfolio.</p>");
-            html.append("        <ul>");
+            cardBuilder.append("    <div class=\"portfolio-card\">");
+            cardBuilder.append("        <h3>").append(portfolio.getName()).append("</h3>");
+            cardBuilder.append("        <p class=\"portfolio-description\">").append(portfolio.getName()).append(" Portfolio.</p>");
+            cardBuilder.append("        <ul>");
 
             for (Investment investment : portfolio.getInvestments()) {
-                html.append("            <li>").append("Asset Class ").append(investment.getId()).append(": ").append(investment.getName()).append("</li>");
+                cardBuilder.append("            <li>").append("Asset Class ").append(investment.getId()).append(": ").append(investment.getName()).append("</li>");
             }
 
-            html.append("        </ul>");
-            html.append("        <div class=\"performance\">");
-            html.append("            <p>Performance: ").append(portfolio.getExpectedReturnOnInvestment()).append("%</p>");
-            html.append("            <button class=\"analysis-button\">Analyze Risk</button>");
-            html.append("        </div>");
-            html.append("    </div>");
+            cardBuilder.append("        </ul>");
+            cardBuilder.append("        <div class=\"performance\">");
+            cardBuilder.append("            <p>Performance: ").append(portfolio.getExpectedReturnOnInvestment()).append("%</p>");
+            cardBuilder.append("            <button class=\"analysis-button\">Analyze Risk</button>");
+            cardBuilder.append("        </div>");
+            cardBuilder.append("    </div>");
         }
 
-        html.append("</div>");
-        html.append("</body>");
-        html.append("</html>");
 
-        return html.toString();
+        return cardBuilder.toString();
     }
 
     @Override
--- src/main/java/com/emmanuel/home/Home.java
+++ src/main/java/com/emmanuel/home/Home.java
@@ -2,8 +2,8 @@ package com.emmanuel.home;
 
 import com.emmanuel.app.bean.PortfolioBeanI;
 import com.emmanuel.app.bean.impl.PortfolioBeanImpl;
+import com.emmanuel.app.model.Portfolio;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,23 +11,104 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
-/**
- * Created by emmanuel on 11/1/23
- *
- * @author: emmanuel
- * @date: 11/1/23
- * @project: IntelliJ IDEA
- */
 @WebServlet("/home")
 public class Home extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PortfolioBeanI portfolioBeanImp = new PortfolioBeanImpl();
+        PortfolioBeanI portfolioBean = new PortfolioBeanImpl();
 
-        String homeInfo = (String) req.getAttribute("homeInfo");
         PrintWriter print = resp.getWriter();
-        print.write(portfolioBeanImp.portfolios());
 
+        print.write("<!DOCTYPE html>");
+        print.write("<html lang=\"en\">");
+        print.write("<head>");
+        print.write("    <meta charset=\"UTF-8\">");
+        print.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        print.write("    <title>Investment App Homepage</title>");
+        print.write("    <style>");
+        print.write("        body {");
+        print.write("            background-color: #f0f0f0;");
+        print.write("            font-family: Arial, sans-serif;");
+        print.write("        }");
+        print.write("        .header {");
+        print.write("            background-color: #007BFF;");
+        print.write("            color: white;");
+        print.write("            padding: 20px;");
+        print.write("            text-align: center;");
+        print.write("        }");
+        print.write("        .welcome {");
+        print.write("            text-align: center;");
+        print.write("            margin: 20px;");
+        print.write("        }");
+        print.write("        .portfolio-container {");
+        print.write("            display: flex;");
+        print.write("            justify-content: space-around;");
+        print.write("            flex-wrap: wrap;");
+        print.write("            padding: 20px;");
+        print.write("        }");
+        print.write("        .portfolio-card {");
+        print.write("            background-color: #fff;");
+        print.write("            border: 1px solid #ddd;");
+        print.write("            border-radius: 5px;");
+        print.write("            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);");
+        print.write("            margin: 10px;");
+        print.write("            width: 300px;");
+        print.write("            padding: 15px;");
+        print.write("        }");
+        print.write("        .portfolio-card h3 {");
+        print.write("            color: #007BFF;");
+        print.write("            font-size: 1.5rem;");
+        print.write("            margin: 0;");
+        print.write("        }");
+        print.write("        .portfolio-description {");
+        print.write("            color: #555;");
+        print.write("            margin: 10px 0;");
+        print.write("        }");
+        print.write("        ul {");
+        print.write("            list-style: none;");
+        print.write("            padding: 0;");
+        print.write("        }");
+        print.write("        li {");
+        print.write("            color: #333;");
+        print.write("            margin: 5px 0;");
+        print.write("        }");
+        print.write("        .performance {");
+        print.write("            display: flex;");
+        print.write("            justify-content: space-between;");
+        print.write("            align-items: center;");
+        print.write("        }");
+        print.write("        .performance p {");
+        print.write("            color: #4CAF50;");
+        print.write("            font-weight: bold;");
+        print.write("        }");
+        print.write("        .analysis-button {");
+        print.write("            background-color: #007BFF;");
+        print.write("            color: white;");
+        print.write("            border: none;");
+        print.write("            border-radius: 5px;");
+        print.write("            padding: 10px 20px;");
+        print.write("            cursor: pointer;");
+        print.write("            transition: background-color 0.3s;");
+        print.write("        }");
+        print.write("        .analysis-button:hover {");
+        print.write("            background-color: #0056b3;");
+        print.write("        }");
+        print.write("    </style>");
+        print.write("</head>");
+        print.write("<body>");
+        print.write("<div class=\"header\">");
+        print.write("    <h1>Welcome Back, Emmanuel Mwiti!</h1>");
+        print.write("    <p>Logged in at (dynamic log time and date)</p>");
+        print.write("</div>");
+        print.write("<div class=\"welcome\">");
+        print.write("    <p>This is your investment dashboard.</p>");
+        print.write("</div>");
+        print.write("<div class=\"portfolio-container\">");
+        print.write(portfolioBean.portfolios());
+        print.write("</div>");
+        print.write("</body>");
+        print.write("</html>");
 
     }
 }
